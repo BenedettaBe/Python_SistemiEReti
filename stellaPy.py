a7fe876d1db9fb8872354ebe68174be040037abe
BenedettaBe <144808596+BenedettaBe@users.noreply.github.com>
2023-10-11 12:23:26 +0200

    caricamento esercizio stella

--- stellaPy.py
+++ stellaPy.py
@@ -8,12 +8,12 @@ tarta = turtle.Turtle()
 tarta.color("red")
 tarta.speed("slow")
 
-gradi = 180 / n
 tarta.begin_fill()
+
+gradi = 180 / n
 for i in range(0,n):
-   if(n % 2 != 0):  
-       tarta.forward(lung)
-       tarta.right(180-gradi)
+    tarta.forward(lung)
+    tarta.right(180-gradi)
 
 tarta.end_fill()
 
